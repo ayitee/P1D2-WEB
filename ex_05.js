746cb4ba63a3478741aa58646c3328ef053c9151
ayitee <matis.gaudin@epitech.digital>
2024-10-01 18:13:55 +0200

    Update ex_05.js

--- ex_05.js
+++ ex_05.js
@@ -1,17 +1,28 @@
 function getBasketContent() {
-  const fruits = ['strawberry', 'strawberry', 'strawberry', 
-                 'apple', 'apple', 
-                 'lime', 'lime', 
-                 'peach', 
-                 'pear', 'pear'];
-
-  console.log(`${fruits.length} fruit(s) selected`);
-  
-  return fruits;
+    let fruits = [];
+    for (let nbfruits = 0; nbfruits < 3; nbfruits++) {
+        fruits.push('strawberry');
+    }
+    for (let nbfruits = 0; nbfruits < 2; nbfruits++) {
+        fruits.push('apple');
+    }
+    for (let nbfruits = 0; nbfruits < 2; nbfruits++) {
+        fruits.push('lime');
+    }
+    fruits.push('peach');
+    for (let nbfruits = 0; nbfruits < 2; nbfruits++) {
+        fruits.push('pear');
+    }
+    console.log(fruits.length + ' fruit(s) selected');
+    return fruits;
 }
 
-// DO NOT TOUCH, this will do the call your function for you and display the fruits if you've done it well
 if (typeof getBasketContent === "function") {
-	const fruits = getBasketContent()
-	showMyBasket(fruits)
+	const fruits = getBasketContent();
+	showMyBasket(fruits);
 }
+
+// A function getBasketContent initializes an empty array 'fruits'.
+// It populates the array with specific fruits using loops for repetition.
+// The total count of fruits is logged, and the array is returned.
+// The final block checks the function's type and calls it, displaying the fruits.
