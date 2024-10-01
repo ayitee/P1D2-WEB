9497c9022f42bb8ae087ae2781eb265acafb84a9
ayitee <matis.gaudin@epitech.digital>
2024-10-01 18:03:05 +0200

    Update ex_02.js

--- ex_02.js
+++ ex_02.js
@@ -1,5 +1,15 @@
 let number = 0
-function addTwo() {
-	number += 2
-  displayThisNumber(number)
+function addTwo(){
+  number += 2
 }
+
+addTwo()
+addTwo()
+addTwo()
+
+displayThisNumber(number);
+
+// Initialize a variable 'number' with the value 0.
+// Define a function 'addTwo' that increments 'number' by 2 each time it is called.
+// Call the 'addTwo' function three times to increment 'number' by 2 each time.
+// Display the current value of 'number' using the displayThisNumber function.
