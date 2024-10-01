25e6c6a93e1a653145fada34aff3903f717bbe74
ayitee <matis.gaudin@epitech.digital>
2024-10-01 15:45:41 +0200

    Update ex_08.js

--- ex_08.js
+++ ex_08.js
@@ -1,1 +1,9 @@
-
+fruits = ["Blackberries", "Mango", "Kiwi", "Peaches", "Strawberry"];
+function getFruit(parameter) {
+    if (typeof parameter === "string") {
+        return fruits.indexOf(parameter);
+    } else if (typeof parameter === "number") {
+        return fruits[parameter] !== undefined ? fruits[parameter] : null;
+    }
+    return null;
+}
