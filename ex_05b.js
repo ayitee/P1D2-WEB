87fa01ca6872fdf8b19d4c0d3d0d7fb98a2266b9
ayitee <matis.gaudin@epitech.digital>
2024-10-01 13:30:25 +0200

    Update ex_05b.js

--- ex_05b.js
+++ ex_05b.js
@@ -1,1 +1,11 @@
-
+function getBasketContent(selectedFruits) {
+  const fruits = ['strawberry', 'strawberry', 'strawberry', 'apple', 'apple', 'lime', 'lime', 'peach', 'pear', 'pear'];
+  if (selectedFruits > fruits.length) {
+    console.log("Too Many fruits selected.");
+    return [];
+  } else {
+    const selected = fruits.slice(0, selectedFruits);
+    console.log(`${selected.length} fruit(s) selected`);
+    return selected;
+  }
+}
