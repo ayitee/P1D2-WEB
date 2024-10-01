378aa2105c0e5cffc6c003da70ae390bd3e1a49c
ayitee <matis.gaudin@epitech.digital>
2024-10-01 15:35:35 +0200

    Update ex_07.js

--- ex_07.js
+++ ex_07.js
@@ -1,1 +1,12 @@
+function getFruitByIndex(index) {
+  if (index === -1) {
+    return fruits[fruits.length - 1];
+  } else if (index < -1 || index >= fruits.length) {
+    return null;
+  }
+  return fruits[index];
+}
 
+displayResult(getFruitByIndex(-1));
+displayResult(getFruitByIndex(3));
+displayResult(getFruitByIndex(10));
