e6f8e5b812dd78a2d4a60a209c027253b293843f
ayitee <matis.gaudin@epitech.digital>
2024-10-01 18:29:35 +0200

    Update ex_08.js

--- ex_08.js
+++ ex_08.js
@@ -1,9 +1,24 @@
 fruits = ["Blackberries", "Mango", "Kiwi", "Peaches", "Strawberry"];
-function getFruit(parameter) {
-    if (typeof parameter === "string") {
-        return fruits.indexOf(parameter);
-    } else if (typeof parameter === "number") {
-        return fruits[parameter] !== undefined ? fruits[parameter] : null;
+
+function getFruit(indexname) {
+    if (indexname === -1 || indexname === "last") {
+        return fruits[fruits.length - 1];
     }
-    return null;
+    if (typeof indexname === 'string') {
+        let index = fruits.indexOf(indexname);
+        return index !== -1 ? index : null;
+    } else if (typeof indexname === 'number') {
+        return fruits[indexname] || null;
+    } else {
+        return null;
+    }
 }
+
+displayResult(getFruit("Mango"));
+// To get the fruit by index, use a number without quotes: displayResult(getFruit(2));
+
+// The function getFruit merges functionalities to handle both fruit names and indices.
+// It returns the index of the fruit name if a string is provided.
+// If an integer is given, it returns the corresponding fruit name.
+// The function also handles special cases for index -1 and "last" to return the last fruit.
+// This approach avoids code duplication and enhances maintainability.
