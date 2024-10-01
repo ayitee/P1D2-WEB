5656ce0d786920c0ea1ba34a7b3a46ad8637631c
ayitee <matis.gaudin@epitech.digital>
2024-10-01 13:00:01 +0200

    Update ex_04.js

--- ex_04.js
+++ ex_04.js
@@ -1,1 +1,20 @@
+let a = 2
+let b = 4
+let action = "sub"
+
+function addtion() {
+	return a + b 
+}
+
+function substraction (){
+	return a - b
+}
+function calculator () {
+	if (action === "add") {
+  return addition()
+  }else if (action === "sub") {
+  	return substraction ()
+  }else {
+  	return 0
+  }
 
