07841de71da77a024a2fb43a162c07c4d5aafcbd
ayitee <matis.gaudin@epitech.digital>
2024-10-01 18:10:36 +0200

    Update ex_01.js

--- ex_01.js
+++ ex_01.js
@@ -1,6 +1,6 @@
-// This function, named sayHi, outputs the string "Hi!" to the console each time it is invoked.
-// The console.log method is used to display the message, and it automatically appends a carriage return (newline) after the output.
-// Each time the function is called, the message "Hi!" will be printed followed by a newline.
 function sayHi() {
   console.log("Hi !")
 }
+// This function, named sayHi, outputs the string "Hi!" to the console each time it is invoked.
+// The console.log method is used to display the message, and it automatically appends a carriage return (newline) after the output.
+// Each time the function is called, the message "Hi!" will be printed followed by a newline.
