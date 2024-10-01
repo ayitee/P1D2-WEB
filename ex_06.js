dc9e7160f7db32414f07fb419f763ae519fdf41f
ayitee <matis.gaudin@epitech.digital>
2024-10-01 15:21:44 +0200

    Update ex_06.js

--- ex_06.js
+++ ex_06.js
@@ -1,1 +1,7 @@
+fruits = ["Blackberries", "Apple", "Banana", "Mango", "Peaches", "Kiwi"];
+function getIndexFromName(fruitName) {
+    return fruits.indexOf(fruitName) !== -1 ? fruits.indexOf(fruitName) : null;
+}
 
+let result = getIndexFromName("Mango");
+displayResult(result);
